b4294a255ea01cfb744f2819f59402d4678cae33
Angel Davila <adavila0703@gmail.com>
2023-05-21 13:50:23 -0500

    adding html content

--- api/directory/router.go
+++ api/directory/router.go
@@ -10,6 +10,7 @@ func SubRoutes(router *gin.RouterGroup, group string) {
 	router.
 		Group(group).
 		GET("GetDirectories", api.Handler(GetDirectories)).
+		POST("GetHtmlContents", api.Handler(GetHtmlContents)).
 		POST("GetRepoCoverage", api.Handler(GetRepoCoverage)).
 		POST("CreateDirectory", api.Handler(CreateDirectory)).
 		POST("CreateDirectories", api.Handler(CreateDirectories))
