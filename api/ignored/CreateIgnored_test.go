109c8631e54980a0cb248545913e801d32ed88aa
Angel Davila <adavila0703@gmail.com>
2023-11-17 14:59:12 -0600

    working on fixing specs

--- api/ignored/CreateIgnored_test.go
+++ api/ignored/CreateIgnored_test.go
@@ -4,11 +4,13 @@ import (
 	"golens-api/api/ignored"
 	"golens-api/clients"
 	"golens-api/coverage"
+	"golens-api/models"
 	"net/http/httptest"
 	"regexp"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"gorm.io/gorm"
@@ -32,23 +34,36 @@ var _ = Describe("CreateIgnored", Ordered, func() {
 		Expect(err).To(BeNil())
 	})
 
-	It("creates ignored", func() {
+	It("creates ignored", Focus, func() {
+		expectedUUID := uuid.New()
 		req := &ignored.CreateIgnoredRequest{
-			Name: "test",
+			Name:        "test",
+			IgnoreType:  "1",
+			DirectoryID: expectedUUID,
 		}
+
+		mock.ExpectQuery(regexp.QuoteMeta(`
+			SELECT * FROM "directories" 
+			WHERE id = $1 AND "directories"."deleted_at" IS NULL
+		`)).WithArgs(
+			expectedUUID,
+		).WillReturnRows(
+			sqlmock.NewRows([]string{"coverage_name"}).AddRow(req.Name),
+		)
 
 		mock.ExpectBegin()
 
 		mock.ExpectExec(regexp.QuoteMeta(`
-			INSERT INTO "ignored_directories" 
-			("id","created_at","updated_at","deleted_at","directory_name") 
-			VALUES ($1,$2,$3,$4,$5)
+			INSERT INTO "ignoreds" ("id","created_at","updated_at","deleted_at","directory_name","name","type") 
+			VALUES ($1,$2,$3,$4,$5,$6,$7)
 		`)).WithArgs(
+			expectedUUID,
 			sqlmock.AnyArg(),
 			sqlmock.AnyArg(),
 			sqlmock.AnyArg(),
-			nil,
 			req.Name,
+			req.Name,
+			models.DirectoryType,
 		).WillReturnResult(sqlmock.NewResult(1, 1))
 
 		mock.ExpectCommit()
